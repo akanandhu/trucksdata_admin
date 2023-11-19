550b3b01a0c598dfbb9760cba22a8a95a4d7a360
Ananthu <akananthu11@gmail.com>
2023-11-20 00:43:51 +0530

    feat: article input

---
+++ src/components/EditorText.tsx
@@ -0,0 +1,48 @@
+import dynamic from 'next/dynamic'
+import React, { SetStateAction } from 'react'
+import 'react-quill/dist/quill.snow.css'
+
+const ReactQuill = dynamic(() => import('react-quill'), { ssr: false })
+
+const EditorText = ({ text, setText }: { text: string; setText: React.Dispatch<SetStateAction<string>> }) => {
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+      [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
+      ['link', 'image'],
+      ['clean'],
+      [{ color: ['black'] }, { background: [] }]
+    ]
+  }
+
+  const formats = [
+    'header',
+    'bold',
+    'italic',
+    'underline',
+    'strike',
+    'blockquote',
+    'list',
+    'bullet',
+    'indent',
+    'link',
+    'image',
+    'color',
+    'background'
+  ]
+
+  const handleChange = (value: string) => {
+    setText(value)
+  }
+
+  return (
+    <>
+      <div className='text-editor'>
+        <ReactQuill theme='snow' modules={modules} formats={formats} onChange={handleChange} value={text} />
+      </div>
+    </>
+  )
+}
+
+export default EditorText
--- src/pages/articles/add/index.tsx
+++ src/pages/articles/add/index.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import { Grid } from '@mui/material'
+import React, { useState } from 'react'
+import 'react-quill/dist/quill.snow.css'
+import EditorText from 'src/components/EditorText'
 
 const ArticlesAdd = () => {
+  const [value, setValue] = useState('')
+
+  console.log(value, 'valueCheck')
+
   return (
-    <div>
-      Hi
-    </div>
+    <Grid sx={{ p: 6, minHeight: '50vh', backgroundColor: 'white' }}>
+      <EditorText text={value} setText={setValue} />
+    </Grid>
   )
 }
 
